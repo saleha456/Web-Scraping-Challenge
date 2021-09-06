052c80d0e3db699df3ed2f806acce68b66064c92
Saleha <salehaahmed456@gmail.com>
2021-09-06 02:14:34 -0400

    scrape mars file done

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,101 @@
+import pandas as pd 
+import requests
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+
+def scrape():
+
+	# Set Executable Path
+	executable_path = {'executable_path': ChromeDriverManager().install()}
+	browser = Browser('chrome', **executable_path, headless=False)
+
+
+	# Scrape NASA Mars News Data
+	mars_news_url = 'https://redplanetscience.com/'
+
+	browser.visit(mars_news_url)
+	time.sleep(2)
+
+	html = browser.html
+	soup = bs(html, 'html.parser')
+
+	news_title = soup.find('div', class_='content_title').text
+	news_p = soup.find('div', class_='article_teaser_body').text
+
+
+
+	# Scrape JPL Mars Space Images
+
+	space_images_url = 'https://spaceimages-mars.com/'
+
+	html = browser.html
+	soup = bs(html, 'html.parser')
+
+	browser.visit(space_images_url)
+	time.sleep(2)
+
+	relative_image_path = soup.find_all('img')[1]["src"]
+	feature_img = space_images_url + relative_image_path
+
+
+	# Scrape Mars Facts Data
+
+	mars_facts_url = 'https://galaxyfacts-mars.com/'
+
+	facts_df = pd.read_html(mars_facts_url, header=0)[0]
+	facts_html = facts_df.to_html()
+
+
+	# Scrape Mars Hemispheres Data
+
+	astrogeology_url = 'https://marshemispheres.com/'
+
+	browser.visit(astrogeology_url)
+	time.sleep(2)
+
+	html = browser.html
+	soup = bs(html, 'html.parser')
+
+	results = soup.find_all('h3')[0:-1]
+
+	image_urls = []
+
+	for result in results:
+	    titles = result.text
+	    browser.links.find_by_partial_text(titles).click()
+	    
+	    html2 = browser.html
+	    soup2 = bs(html2, 'html.parser')
+	    
+	    title = soup2.find('h2', class_='title').text
+	    title = title.replace("Enhanced","").strip()
+	    
+	    sample = soup2.find('li')
+	    imgurl = sample.find('a')['href']
+	    imgurl = astrogeology_url + imgurl
+	    
+	    hemisphere_dict = {"title" : title, "img_url" : imgurl}
+	    image_urls.append(hemisphere_dict)
+	    
+	    browser.links.find_by_partial_text('Back').click()
+	
+	# Create Dictionary for all scraped data
+	scrape_data = {
+		"news_title":news_title,
+		"news_paragraph":news_p,
+		"feature_image":feature_img,
+		"mars_facts":facts_html,
+		"hemispheres":image_urls
+	}
+
+	# Close Browser
+	browser.quit()
+
+	# Return Data
+	return scrape_data
+
+
+
